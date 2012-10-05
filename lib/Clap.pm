038efbc2b84d6d807a299c72d2a5c6f4d490b9b2
Kan Fushihara <kan@fout.jp>
2012-10-05 18:03:59 +0900

    implement basic concept

--- lib/Clap.pm
+++ lib/Clap.pm
@@ -4,6 +4,36 @@ use warnings;
 use 5.010001;
 our $VERSION = '0.01';
 
+use Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(app);
+
+use UNIVERSAL::can;
+
+sub app {
+    my $pkg = shift;
+
+    sub {
+        my $env = shift;
+
+        my ($method,) = grep { $_ } split(qr{/}, $env->{PATH_INFO}||'');
+        $method ||= 'index'; # default
+        if ($pkg->can($method)) {
+            return [
+                200,
+                [ 'Content-Type' => 'text/html' ],
+                [ $pkg->$method($env) ]
+            ];
+        } else {
+            return [
+                404,
+                [ 'Content-Type' => 'text/html' ],
+                [ "Not Found" ]
+            ];
+        }
+    };
+}
 
 
 1;
